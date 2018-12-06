57f6d64b0f433d8b52b415071a761db5a74f8528
irradiantlife <stopforgetting@gmail.com>
2018-12-06 00:20:30 -0800

    fixed API_server to not be blocking

--- api_server.py
+++ api_server.py
@@ -1,5 +1,17 @@
 import socket
 import threading
+import SocketServer
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024)
+        cur_thread = threading.current_thread()
+        response = "{}: {}".format(cur_thread.name, data)
+        self.request.sendall(response)
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
 
 class API_Server:
 
@@ -7,26 +19,7 @@ class API_Server:
        print('entered API_Server init with port: ?', [port])
        self.db = database
 
-       bind_ip = '0.0.0.0'
-
-       self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-       self.server.bind((bind_ip, port))
-       self.server.listen(5)  # max backlog of connections
-
-       print 'Listening on {}:{}'.format(bind_ip, port)
-
-       while True:
-           client_sock, address = self.server.accept()
-           print 'Accepted connection from {}:{}'.format(address[0], address[1])
-           client_handler = threading.Thread(
-               target=handle_client_connection,
-               args=(client_sock,)  # without comma you'd get a... TypeError: handle_client_connection() argument after * must be a sequence, not _socketobject
-           )
-           client_handler.start()
-
-
-    def handle_client_connection(client_socket):
-        request = client_socket.recv(1024)
-        print 'Received {}'.format(request)
-        client_socket.send('ACK!')
-        client_socket.close()
+       server = ThreadedTCPServer(("localhost", port), ThreadedTCPRequestHandler)
+       server_thread = threading.Thread(target=server.serve_forever)
+       server_thread.daemon = True
+       server_thread.start()

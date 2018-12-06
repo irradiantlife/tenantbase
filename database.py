d96b05a2ee50499b1acea76f46a0294609a13db3
irradiantlife <stopforgetting@gmail.com>
2018-12-06 09:51:19 -0800

    in WebServer, ovveride anything that says "json" with the json output. Added support in database. I figured it would be fine to call the a endpoint from the one-page app rather than have a wierd injection point and database hit on the initial page load.

--- database.py
+++ database.py
@@ -6,11 +6,19 @@ class Database:
 
         self.db = sqlite3.connect('tb-keyvalue-store.db')
 
+        #let's just always be a python dictionary
+        def dict_factory(cursor, row):
+           d = {}
+           for idx, col in enumerate(cursor.description):
+               d[col[0]] = row[idx]
+           return d
+        self.db.row_factory = dict_factory
+
         self.db.execute('CREATE TABLE IF NOT EXISTS storage (key TEXT PRIMARY KEY, value TEXT)')
 
         #print all existing keys, for debugging, since it was explicitly permitted
-        for row in self.db.execute('SELECT * from storage').fetchall():
-            print(row)
+        #for row in self.db.execute('SELECT * from storage').fetchall():
+        #    print(row)
 
 
     def put_value(self, key, value):
@@ -22,7 +30,7 @@ class Database:
         print 'key', key
         value = ""
         for row in self.db.execute('SELECT value FROM storage WHERE key = ?', [key]).fetchall():
-            value = row[0]
+            value = row['value']
             return value #row #exiting on 1st on purpose
         return value
 
--- web_server.py
+++ web_server.py
@@ -2,12 +2,25 @@ import SimpleHTTPServer
 import SocketServer
 import threading
 from database import Database
+import json
 
 
 class ThreadedSimpleHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
     def __init__(self, *args, **keys):
         self.db = Database()
-        SimpleHTTPServer.SimpleHTTPRequestHandler__init__(self, *args, **keys)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, *args, **keys)
+
+    def do_GET(self):
+        if (self.path.endswith('json')):
+            rawdata = self.db.read_all()
+            content = json.dumps([dict(ix) for ix in rawdata])
+
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            self.wfile.write(content)
+            return
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass

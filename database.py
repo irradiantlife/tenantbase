da2bba42ccd638d361d45ec7fbb44b52e8a67c31
irradiantlife <stopforgetting@gmail.com>
2018-12-06 02:56:58 -0800

    implementing sql

--- database.py
+++ database.py
@@ -8,23 +8,27 @@ class Database:
 
         self.cursor = self.db.cursor()
 
-        self.cursor.execute('CREATE table IF NOT EXISTS storage (key text, value text)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS storage (key TEXT PRIMARY KEY, value TEXT)')
 
         #print all existing keys, for debugging, since it was explicitly permitted
         for row in self.cursor.execute('SELECT * from storage'):
             print(row)
 
 
-    def put_value(key, value):
-        print('key: ? value?', [key], [value])
+    def put_value(self, key, value):
+        print 'key', key, 'value', value
+        self.cursor.execute('REPLACE INTO storage (key, value) VALUES (?, ?)', [key,value])
 
-    #def read(key):
+    def read(self, key):
+        print 'key', key
+        for row in self.cursor.execute('SELECT value FROM storage WHERE key = ?', [key]):
+            print row
+            return row #exiting on 1st on purpose
 
-
-    #def read_all():
-    #ok
-
-
-    def delete(key):
-        print([key])
+    def delete(self, key):
+        print 'deleting ', key
         self.cursor.execute('delete from storage where key = ?', [key])
+
+    def read_all(self):
+        #what kind of struct is this even
+        return self.cursor.execute('SELECT key, value FROM storage')

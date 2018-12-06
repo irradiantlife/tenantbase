bf9eef9fc5fdc0344bc0134fe75a050f2c3c042e
irradiantlife <stopforgetting@gmail.com>
2018-12-06 00:56:25 -0800

    implemented Web_Server

--- web_server.py
+++ web_server.py
@@ -1,7 +1,23 @@
+import SimpleHTTPServer
+import SocketServer
+import threading
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
 
 class Web_Server:
 
-   def __init__(self, database, port):
-       print('entered Web_Server init with port:?', [port])
+    def __init__(self, database, port):
+        print "entered Web_Server init with port:", port
 
-       self.db = database
+        self.db = database
+
+        Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+        server = ThreadedTCPServer(("", port), Handler)
+
+        server_thread = threading.Thread(target=server.serve_forever)
+        server_thread.daemon = True
+        server_thread.start()
